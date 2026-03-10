16e6db4ea7128a67f17c6585465a70d472bf6a42
LuluDiegs <luluca.diegues@gmail.com>
2026-03-10 04:53:42 -0300

    banco de dados

--- PlantaCoreAPI.API/Extensions/DatabaseExtensions.cs
+++ PlantaCoreAPI.API/Extensions/DatabaseExtensions.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using PlantaCoreAPI.Infrastructure.Dados;
-using System.Net;
-using System.Net.Sockets;
 
 namespace PlantaCoreAPI.API.Extensions;
 
@@ -12,36 +10,9 @@ internal static class DatabaseExtensions
         var connectionString = configuration.GetConnectionString("DefaultConnection")
             ?? throw new InvalidOperationException("ConnectionString 'DefaultConnection' não configurada.");
 
-        connectionString = ResolverIpv4(connectionString);
-
         services.AddDbContext<PlantaCoreDbContext>(opcoes =>
             opcoes.UseNpgsql(connectionString));
 
         return services;
-    }
-
-    private static string ResolverIpv4(string connectionString)
-    {
-        try
-        {
-            var builder = new Npgsql.NpgsqlConnectionStringBuilder(connectionString);
-            var host = builder.Host;
-
-            if (string.IsNullOrEmpty(host) || IPAddress.TryParse(host, out _))
-                return connectionString;
-
-            var enderecos = Dns.GetHostAddresses(host);
-
-            var ipv4 = enderecos.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
-
-            if (ipv4 != null)
-                builder.Host = ipv4.ToString();
-
-            return builder.ToString();
-        }
-        catch
-        {
-            return connectionString;
-        }
     }
 }
