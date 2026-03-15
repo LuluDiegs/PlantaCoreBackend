a3af40bda160e800fc3763b9af43272e26ff85b0
MauroRaya <maurorayabr@gmail.com>
2026-03-15 19:54:07 -0300

    chore(evento): roda migrations para executar alteracoes dos eventos no banco

---
+++ PlantaCoreAPI.Infrastructure/Migrations/20260315210536_AddEventos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlantaCoreAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "eventos",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    titulo = table.Column<string>(type: "text", nullable: false),
+                    descricao = table.Column<string>(type: "text", nullable: false),
+                    localizacao = table.Column<string>(type: "text", nullable: false),
+                    data_inicio = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    anfitriao_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_eventos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_eventos_usuarios_anfitriao_id",
+                        column: x => x.anfitriao_id,
+                        principalTable: "usuarios",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "evento_participantes",
+                columns: table => new
+                {
+                    EventoId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_evento_participantes", x => new { x.EventoId, x.UsuarioId });
+                    table.ForeignKey(
+                        name: "FK_evento_participantes_eventos_EventoId",
+                        column: x => x.EventoId,
+                        principalTable: "eventos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_evento_participantes_usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "usuarios",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_evento_participantes_UsuarioId",
+                table: "evento_participantes",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_eventos_anfitriao_id",
+                table: "eventos",
+                column: "anfitriao_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "evento_participantes");
+
+            migrationBuilder.DropTable(
+                name: "eventos");
+        }
+    }
+}
