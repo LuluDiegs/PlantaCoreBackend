791d8beec5d25b7f9355a74e07ee7391af3e9b1b
LuluDiegs <luluca.diegues@gmail.com>
2026-03-19 00:15:51 -0300

    Arrumando cors

--- PlantaCoreAPI.API/Program.cs
+++ PlantaCoreAPI.API/Program.cs
@@ -32,7 +32,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:3000",
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "https://localhost:7123"
+                "https://localhost:7123",
+                frontendUrl
             );
         }
         else
