c71b91f1f48ffe61a2c0740e78c51c4431756cac
LuluDiegs <luluca.diegues@gmail.com>
2026-03-22 02:15:53 -0300

    Testando swagger

--- PlantaCoreAPI.API/Program.cs
+++ PlantaCoreAPI.API/Program.cs
@@ -114,15 +114,13 @@ builder.Services.AddScoped<IRepositorioEvento, RepositorioEvento>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlantaCoreAPI v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlantaCoreAPI v1");
+    c.RoutePrefix = string.Empty;
+});
+
 
 app.UseCors("AllowFrontend");
 app.UseAuthentication();
