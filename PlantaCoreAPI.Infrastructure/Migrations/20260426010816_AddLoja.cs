96afd3118895e15c6c9080dabc4a1c4ef97bf70e
MauroRaya <maurorayabr@gmail.com>
2026-04-25 22:10:51 -0300

    feat: adiciona tabela loja

---
+++ PlantaCoreAPI.Infrastructure/Migrations/20260426010816_AddLoja.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlantaCoreAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoja : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "lojas",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    nome = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    descricao = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    telefone = table.Column<string>(type: "text", nullable: true),
+                    imagem_url = table.Column<string>(type: "text", nullable: true),
+                    somente_online = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    cidade = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    estado = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    endereco = table.Column<string>(type: "text", nullable: true),
+                    usuario_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_lojas", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_lojas_usuario",
+                        column: x => x.usuario_id,
+                        principalTable: "usuarios",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_lojas_usuario_id",
+                table: "lojas",
+                column: "usuario_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "lojas");
+        }
+    }
+}
